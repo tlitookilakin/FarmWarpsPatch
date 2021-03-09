b480b6d5b1d901e4050c7aafcd2978350e4f490a
Wren <Wren@MYSTERY-SHACK>
2021-03-09 13:59:57 -0500

    initial commit

---
+++ FarmWarpsPatch/ModEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Harmony;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace FarmWarpsPatch
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            //send monitor to patch for logging
+            Patch.initialize(Monitor);
+
+            //setup harmony
+            var harmony = HarmonyInstance.Create(this.ModManifest.UniqueID);
+
+            //patch island obelisk warp
+            harmony.Patch(
+               original: AccessTools.Method(typeof(StardewValley.Locations.IslandWest), nameof(StardewValley.Locations.IslandWest.performAction)),
+               prefix: new HarmonyMethod(typeof(Patch), nameof(Patch.island_performAction_prefix))
+            );
+        }
+    }
+}
